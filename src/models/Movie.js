7639b956473e94acee470fd959110ec731b3b897
Bobbywestrom <bobby@westromsoftware.com>
2022-10-14 17:05:54 -0400

    Added create, update, delete functions to movie model

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -16,4 +16,41 @@ export default class Movie extends Model {
   @date('release_date_at') releaseDateAt;
 
   @children('reviews') reviews;
+
+  getMovie() {
+    return {
+      title: this.title,
+      posterImage: this.posterImage,
+      genre: this.genre,
+      description: this.description,
+      releaseDateAt: this.releaseDateAt,
+    };
+  }
+
+  async addReview(body) {
+    return this.collections.get('reviews').create((review) => {
+      review.movie.set(this);
+      review.body = body;
+    });
+  }
+
+  updateMovie = async (updatedMovie) => {
+    await this.update((movie) => {
+      movie.title = updatedMovie.title;
+      movie.genre = updatedMovie.genre;
+      movie.posterImage = updatedMovie.posterImage;
+      movie.description = updatedMovie.description;
+      movie.releaseDateAt = updatedMovie.releaseDateAt;
+    });
+  };
+
+  async deleteAllReview() {
+    await this.reviews.destroyAllPermanently();
+  }
+
+  async deleteMovie() {
+    await this.deleteAllReview();
+    await this.markAsDeleted();
+    await this.destroyPermanently();
+  }
 }
