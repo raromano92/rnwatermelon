e2aaa313e448084432a304b3d74358297a2fa867
Bobbywestrom <bobby@westromsoftware.com>
2022-10-14 12:23:00 -0400

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'ignore',
+  jsxBracketSameLine: true,
+  printWidth: 100,
+  proseWrap: 'preserve',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
